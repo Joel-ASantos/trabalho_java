945d874f76b25b38f9ce70c895e8113ada108269
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-04-25 17:17:07 -0300

    add locks in banco file

--- trabalho/sistema_bancario/Banco.java
+++ trabalho/sistema_bancario/Banco.java
@@ -1,5 +1,10 @@
 package trabalho.sistema_bancario;
-
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 public class Banco {
-    
+    private static Lock lock = new ReentrantLock(); // add lock
+
+    public static void transferencia(Conta cliente,Conta loja, float valor){
+
+    }
 }
