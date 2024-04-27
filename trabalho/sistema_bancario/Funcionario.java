4942bace39623cd4792f90c2cea64a7abde7379d
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-04-27 01:35:41 -0300

    add

--- trabalho/sistema_bancario/Funcionario.java
+++ trabalho/sistema_bancario/Funcionario.java
@@ -3,16 +3,21 @@ package trabalho.sistema_bancario;
 public class Funcionario extends Thread {
     Conta conta_funcionario;
     private Conta conta_investimento;
+    private double valor_retirado;
     Banco banco;
     
-    public Funcionario(Conta conta_funcionario,Conta conta_investimento){
-        this.conta_funcionario = new Conta(MIN_PRIORITY, MAX_PRIORITY);
-        this.conta_investimento = new Conta(MIN_PRIORITY, MAX_PRIORITY);
+    public Funcionario(Conta conta_funcionario,Conta conta_investimento,double valor_retirado){
+        this.conta_funcionario = conta_funcionario;
+        this.conta_investimento = conta_investimento;
         this.conta_funcionario.saldo = 0;
         this.conta_investimento.saldo = 0;
+        this.valor_retirado = valor_retirado;
     }
 
     public void run(){
-
+        if(conta_funcionario.saldo == 1400.00){
+            valor_retirado = conta_funcionario.saldo * 0.20;
+            banco.transferencia(conta_funcionario, conta_investimento, valor_retirado);
+        }
     }
 }
